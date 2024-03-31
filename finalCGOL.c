29a7ef6360dcbf1f2bf6d864c3b9437340233780
allenly3 <30699006+allenly3@users.noreply.github.com>
2024-03-31 15:43:55 -0400

    Ini p0 matrix

--- finalCGOL.c
+++ finalCGOL.c
@@ -12,10 +12,10 @@ int main(int argc, char* argv[]) {
     // Only 0 and 1, so use int
 
     // To be periodic, originally
-    /// we generate an N+1 x N+1 matrix
+    /// we generate an N+2 x N+2 matrix
 
-    int A[N+1][N+1];
-    int T[N+1][N+1];
+    int A[N+2][N+2];
+    int T[N+2][N+2];
     MPI_Status status;
     MPI_Datatype column_mpi_t;
     int i, j;
@@ -31,27 +31,39 @@ int main(int argc, char* argv[]) {
 
     if (my_rank == 0) {
 
-      for (i = 0; i < N; i++){
-        for (j = 0; j < N; j++){
-          A[i][j] =  rand() % 2;
+      for (i = 0; i < N+2; i++){
+        for (j = 0; j < N+2; j++){
+          // The edge value init to 8
+          if(i == 0 || j == 0 || i == N+1 || j == N+1){
+            A[i][j] = 8;
+          }else{
+            A[i][j] =  A[i][j] =  rand() % 2;;
+          }   
         }
       }
-            
-        MPI_Send(&(A[0][4]), 1, column_mpi_t, 1, 0,MPI_COMM_WORLD);
-    
-    } else { 
-        for (i = 0; i < N; i++)
-            for (j = 0; j < N; j++)
-                T[i][j] = 0;
 
-        MPI_Recv(&(T[0][4]), 1, column_mpi_t, 0, 0,MPI_COMM_WORLD, &status);
-        for (i = 0; i < N; i++) {
-          for (j = 0; j < N; j++){
-            printf("%d ", T[i][j]);
-          }
-          printf("\n");
+      for (i = 0; i < N+2; i++){
+        for (j = 0; j < N+2; j++){
+           printf("%d ", A[i][j]);
         }
         printf("\n");
+      }
+            
+        //MPI_Send(&(A[0][4]), 1, column_mpi_t, 1, 0,MPI_COMM_WORLD);
+    
+    } else { 
+        // for (i = 0; i < N; i++)
+        //     for (j = 0; j < N; j++)
+        //         T[i][j] = 0;
+
+        // MPI_Recv(&(T[0][4]), 1, column_mpi_t, 0, 0,MPI_COMM_WORLD, &status);
+        // for (i = 0; i < N; i++) {
+        //   for (j = 0; j < N; j++){
+        //     printf("%d ", T[i][j]);
+        //   }
+        //   printf("\n");
+        // }
+        // printf("\n");
     }
 
     MPI_Finalize();
