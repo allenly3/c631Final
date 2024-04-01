4bba8c810ea7c536220d63aacfa958f843f6241e
allenly3 <30699006+allenly3@users.noreply.github.com>
2024-04-01 00:02:21 -0400

    Init full periodic Matrix in P0

--- finalCGOL.c
+++ finalCGOL.c
@@ -2,7 +2,7 @@
 #include "stdlib.h"
 #include "mpi.h"
 #include "time.h"
-#define N 16
+#define N 4
 
 int main(int argc, char* argv[]) {
     int p;
@@ -12,13 +12,14 @@ int main(int argc, char* argv[]) {
     // Only 0 and 1, so use int
 
     // To be periodic, originally
-    /// we generate an N+2 x N+2 matrix
 
     int A[N+2][N+2];
     int T[N+2][N+2];
     MPI_Status status;
     MPI_Datatype column_mpi_t;
-    int i, j;
+    int i, j, counter;
+    counter = 10;
+
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
@@ -29,15 +30,51 @@ int main(int argc, char* argv[]) {
     MPI_Type_commit(&column_mpi_t);
     srand(time(NULL));
 
+    for (i = 0; i < N+2; i++){
+      for (j = 0; j < N+2; j++){ 
+        A[i][j] = 99;
+      }
+    }
+
     if (my_rank == 0) {
 
       for (i = 1; i < N+1; i++){
         for (j = 1; j < N+1; j++){
-          A[i][j] =  A[i][j] =  rand() % 2;
+          //A[i][j] =  A[i][j] =  rand() % 2;
+          A[i][j] =  A[i][j] =  counter;
+          counter++;
 
           // assign edge and corner values
-          if(i == 1 && j == 1){
-            A[N+1][N+1] = A[i][j];
+          if(i == 1){
+            //First row value to 
+            if(j == 1){
+              A[N+1][N+1] = A[i][j];
+            }
+            if(j == N){
+              A[N+1][0] = A[i][j];
+            }
+            A[N+1][j] =  A[i][j];
+            
+          }
+
+          if( i == N ){
+
+            if(j == 1){
+              A[0][N+1] = A[i][j];
+            }
+            if(j == N){
+              A[0][0] = A[i][j];
+            }
+            A[0][j] =  A[i][j];
+
+          }
+
+          if(j == 1){
+            A[i][N+1] = A[i][j];
+          }
+
+          if(j == N){
+            A[i][0] = A[i][j];
           }
  
              
