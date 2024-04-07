74c32e704516d1b1f1b08a1624d306297aa1007b
allenly3 <30699006+allenly3@users.noreply.github.com>
2024-04-07 18:56:37 -0400

    send matrix to other p

--- finalCGOL.c
+++ finalCGOL.c
@@ -3,12 +3,15 @@
   - Only 0 and 1, so use INT array
   - 0 stands for dead, 1 stands for alive
   - N for matrix edge size
-  - We assume that  N is divisible by p ( the number of process)  
+  - We assume that  N is divisible by p 
+  - Ansume that size  = p * p  
 */
 #include "stdio.h"
 #include "stdlib.h"
 #include "mpi.h"
 #include "time.h"
+#include "unistd.h"
+
 #define N 8
 
 int main(int argc, char* argv[]) {
@@ -24,13 +27,15 @@ int main(int argc, char* argv[]) {
     P 0 1 0 1 P
     P P P P P P
   So the array initialized size is N+2
+  Same as in submatrix
   */
 
   int A[N+2][N+2];
   int T[N+2][N+2];
+  int S[N+2][N+2];
   MPI_Status status;
   MPI_Datatype column_mpi_t;
-  int i, j, counter;
+  int i, j, k, q, counter, row , col;
   int G = 1; // Number of Generation we want to see
   int subSize; 
   counter = 10;
@@ -47,12 +52,15 @@ int main(int argc, char* argv[]) {
     return 0;
   }
 
+  row = size;
+  col = size;
+  
   // Split Original matrix A according to size P
   // And create a vector of sending blocks
 
   subSize = N/size;  // number of element in submatrix
 
-  MPI_Type_vector(subSize, subSize, N+2, MPI_INT, &column_mpi_t);
+  MPI_Type_vector(subSize+2, subSize+2, N+2, MPI_INT, &column_mpi_t);
   MPI_Type_commit(&column_mpi_t);
 
   
@@ -118,32 +126,47 @@ int main(int argc, char* argv[]) {
     }
 
     printf("************ Initialization Done ************\n");      
-      //MPI_Send(&(A[0][4]), 1, column_mpi_t, 1, 0,MPI_COMM_WORLD);
+   
+    // Sending submatrix
 
+    for (i = 0 ; i < size ; i ++){
+      for(j = 0; j < size; j ++){
+          if(j == 0 ){
+            continue; //  Skip p0 
+          }
+          MPI_Send(&(A[i*subSize][j*subSize]), 1, column_mpi_t, j, 0, MPI_COMM_WORLD);
 
+      }
+    }
 
-
-    
 
 
 
   
+  
   } else { 
-      // for (i = 0; i < N; i++)
-      //     for (j = 0; j < N; j++)
-      //         T[i][j] = 0;
+      for (i = 0; i < N+2; i++)
+          for (j = 0; j < N+2; j++)
+              T[i][j] = 0;
+      
+      for( k =0; k < size; k++){
+        MPI_Recv(&(T[0][0]), 1, column_mpi_t, 0, 0,MPI_COMM_WORLD, &status);
+        usleep(100000);
+        for (i = 0; i < subSize+2; i++) {
+          for (j = 0; j < subSize+2; j++){
+            printf("%d ", T[i][j]);
+          }
+          printf("\n");
+        }
+        printf("\n");
+      }
+      
 
-      // MPI_Recv(&(T[0][4]), 1, column_mpi_t, 0, 0,MPI_COMM_WORLD, &status);
-      // for (i = 0; i < N; i++) {
-      //   for (j = 0; j < N; j++){
-      //     printf("%d ", T[i][j]);
-      //   }
-      //   printf("\n");
-      // }
-      // printf("\n");
   }
 
   MPI_Finalize();
   return 0 ; 
 }  /* main */
 	
+
+
--- test.c
+++ test.c
@@ -1,30 +1,47 @@
-/*
-    Read Me
-  - Only 0 and 1, so use INT array
-  - 0 stands for dead, 1 stands for alive
-  - N for matrix edge size
-  - We assume that  N is divisible by p ( the number of process)  
-  - p also is a square number
-*/
-#include "stdio.h"
-#include "stdlib.h"
-#include "mpi.h"
-#include "time.h"
-#define N 4
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
 
-int main(int argc, char* argv[]) {
+#define ROWS 2
+#define COLS 6
 
+int main(int argc, char *argv[]) {
+    MPI_Init(&argc, &argv);
 
-    // Print the first command-line argument if it exists
-    if (argc > 1) {
-        printf("First command-line argument: %s\n", argv[1]);
-    } else {
-        printf("No command-line arguments provided.\n");
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if (size != 3) {
+        printf("This program requires exactly 3 processes.\n");
+        MPI_Abort(MPI_COMM_WORLD, 1);
     }
 
+    MPI_Datatype column_mpi_t;
+    MPI_Type_vector(2, 2, COLS, MPI_INT, &column_mpi_t);
+    MPI_Type_commit(&column_mpi_t);
 
- 
-    return 0 ; 
+    int root = 0;
+    int sendbuf[ROWS][COLS] = {
+        {1, 2, 3, 4, 5, 6},
+        {7, 7, 8, 8, 9, 9}
+    };
 
+    int recvbuf[2][2];
 
+    // Scatter the data from the root process to all other processes
+    MPI_Scatter(sendbuf, 4, MPI_INT, recvbuf, 4, MPI_INT, root, MPI_COMM_WORLD);
+
+    // Print out the received data on each process
+    printf("Process %d received:\n", rank);
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("%d ", recvbuf[i][j]);
+        }
+        printf("\n");
+    }
+
+    MPI_Type_free(&column_mpi_t);
+    MPI_Finalize();
+    return 0;
 }
